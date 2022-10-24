07ecbcff0ebab8e7bc0b27068a9a6cd7475f6a0a
Abnêr Borgonha <abnerborgonha@gmail.com>
2022-10-24 18:14:07 -0300

    feat: link button register user from login

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-function redirecionar(){
-    window.location("login.html");
+const buttonRegisterUser = document.getElementById("buttonRegisterUser");
+
+function linkTo(link) {
+  const { origin } = window.location;
+  window.location.href = `${origin}/${link}.html`;
 }
+
+buttonRegisterUser.addEventListener("click", (event) => {
+  event.preventDefault();
+  linkTo("login");
+});
